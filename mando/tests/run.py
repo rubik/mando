24a05047b37f615e97daa99ef02aac73c096e423
Krisztián Fekete <fekete.krisztyan@gmail.com>
2014-08-07 10:01:17 +0200

    failing test

--- mando/tests/run.py
+++ mando/tests/run.py
@@ -4,5 +4,6 @@ if __name__ == '__main__':
     import unittest
     from mando.tests.test_core import *
     from mando.tests.test_utils import *
+    from mando.tests.test_unicode_docstring_on_py2 import *
 
     unittest.main()
