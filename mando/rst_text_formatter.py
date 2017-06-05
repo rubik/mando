f9537cc3348f0d7f80544786bcbba3e1c9e01784
Michele Lacchia <michelelacchia@gmail.com>
2017-06-05 08:48:11 +0200

    fix(napoleon): remove Sphinx dependencies

--- mando/rst_text_formatter.py
+++ mando/rst_text_formatter.py
@@ -17,16 +17,16 @@ def b(s):
 
 class RSTHelpFormatter(argparse.RawTextHelpFormatter):
     """
+    Custom formatter class that is capable of interpreting ReST.
     """
     def format_help(self):
-        ret = rst2ansi(b(super(RSTHelpFormatter, self).format_help())
-                       + b('\n'))
+        ret = rst2ansi(b(super(RSTHelpFormatter, self).format_help()) +
+                       b('\n'))
         return ret.encode(sys.stdout.encoding,
                           'replace').decode(sys.stdout.encoding)
 
     def format_usage(self):
-        ret = rst2ansi(b(super(RSTHelpFormatter, self).format_usage())
-                       + b('\n'))
+        ret = rst2ansi(b(super(RSTHelpFormatter, self).format_usage()) +
+                       b('\n'))
         return ret.encode(sys.stdout.encoding,
                           'replace').decode(sys.stdout.encoding)
-
