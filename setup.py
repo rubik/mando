e4794ec4000ec0c417d1d19bc244637203bbfbc7
Emil Tylen <emil.tylen@skf.com>
2020-03-02 09:25:25 +0100

    CI and dependency changes
    * Removed deprecated python3.4 from CI runs
    * Moved actions from install-26-deps.sh to setup.py
    * Added checks for different setuptools versions to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import os
-from setuptools import setup, find_packages
+
+import setuptools
+
 try:
     import mando
 except ImportError as e:
@@ -7,49 +9,62 @@ except ImportError as e:
 else:
     version = mando.__version__
 
-deps = [
-    'six',
-    'argparse ; python_version<="2.6"',
-    'funcsigs ; python_version<="3.2"',
-]
+deps = ["six"]
+extras = {"restructuredText": ["rst2ansi"]}
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as fobj:
+
+sversion = setuptools.__version__.split(".")
+
+if sversion > (36, 2):
+    deps += ['argparse ; python_version<="2.6"', 'funcsigs ; python_version<="3.2"']
+elif sversion > (18, 0):
+    extras[':python_version<="3.2"'] = ["argparse"]
+    extras[':python_version<="2.7"'] = ["funcsigs"]
+
+
+with open(os.path.join(os.path.dirname(__file__), "README.rst")) as fobj:
     readme = fobj.read()
 
-setup(name='mando',
-      version=version,
-      author='Michele Lacchia',
-      author_email='michelelacchia@gmail.com',
-      url='https://mando.readthedocs.org/',
-      download_url='https://pypi.python.org/mando/',
-      license='MIT',
-      description='Create Python CLI apps with little to no effort at all!',
-      platforms='any',
-      long_description=readme,
-      packages=find_packages(),
-      install_requires=deps,
-      extras_require={'restructuredText': ['rst2ansi'],},
-      test_suite='mando.tests',
-      keywords='argparse,argument parser,arguments,cli,command line,'
-               'commands,decorator,dispatch,flags,getopt,options,optparse,'
-               'parser,subcommands',
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: Console',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Natural Language :: English',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.1',
-          'Programming Language :: Python :: 3.2',
-          'Programming Language :: Python :: 3.3',
-          'Topic :: Software Development',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-          'Topic :: Utilities',
-      ]
+setuptools.setup(
+    name="mando",
+    version=version,
+    author="Michele Lacchia",
+    author_email="michelelacchia@gmail.com",
+    url="https://mando.readthedocs.org/",
+    download_url="https://pypi.python.org/mando/",
+    license="MIT",
+    description="Create Python CLI apps with little to no effort at all!",
+    platforms="any",
+    long_description=readme,
+    packages=setuptools.find_packages(),
+    install_requires=deps,
+    extras_require=extras,
+    test_suite="mando.tests",
+    keywords="argparse,argument parser,arguments,cli,command line,"
+    "commands,decorator,dispatch,flags,getopt,options,optparse,"
+    "parser,subcommands",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
 )
