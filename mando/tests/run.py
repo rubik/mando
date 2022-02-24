51cb2fd00c0536731eb08ae124155f40dd7948bc
Michele Lacchia <michelelacchia@gmail.com>
2022-02-24 09:00:53 +0100

    chore(tests): update pytest arguments

--- mando/tests/run.py
+++ mando/tests/run.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 
 if __name__ == '__main__':
+    import sys
     import pytest
 
-    pytest.main(['--strict'])
+    if sys.version_info[:2] >= (3, 10):
+        pytest.main(['--strict-markers'])
+    else:
+        pytest.main(['--strict'])
